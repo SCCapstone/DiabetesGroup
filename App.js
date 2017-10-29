38d438d8e1808d61549f3ef62791e77d249c23e6
Eric R Mims <emims@email.sc.edu>
2017-10-29 14:30:50 -0400

    Add simple text to App.js for SC milestone.

--- App.js
+++ App.js
@@ -32,6 +32,9 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
+	    <Text style = {styles.instructions}>
+	    	Mims SourceControl Milestone.
+	    </Text>
       </View>
     );
   }
