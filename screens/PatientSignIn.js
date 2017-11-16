71185d25adf2a5a3a8bb9db5b0c75547835bb670
Eric R Mims <emims@email.sc.edu>
2017-11-16 09:32:27 -0500

    Added navigation to patient home screen.

--- screens/PatientSignIn.js
+++ screens/PatientSignIn.js
@@ -43,7 +43,7 @@ export default class PatientSignIn extends Component<{}> {
 
                     <SeafoamButton
                         title="LOGIN"
-
+			onPress={() => navigate('PHome')}
                     />
 
                 </View>
