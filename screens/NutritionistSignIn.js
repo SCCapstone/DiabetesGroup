d5253f9b4e75a3c58e564572bf8bbf497f6e0c81
James M Chaszar <jchaszar@email.sc.edu>
2017-11-29 03:48:22 -0500

    Added sign in functionality using firebase

--- screens/NutritionistSignIn.js
+++ screens/NutritionistSignIn.js
@@ -15,6 +15,47 @@ export default class NutritionistSignIn extends Component<{}> {
 
 	static navigationOptions = {title: 'Please input your login credentials',};
 
+	constructor(props){
+	    super(props);
+	    this.state =  {email: '', password: ''};
+    }
+
+    _signIn(){
+	    var email = this.state.email;
+	    var password = this.state.password;
+        const {navigate} = this.props.navigation;
+
+	    if(email.length < 4){
+	        alert('Please enter a valid email address.');
+        }
+        if(password.length < 4){
+	        alert('Please enter a valid password');
+        }
+        //Log in user if correct credentials are entered
+        firebaseApp.auth().signInWithEmailAndPassword(email,password)
+            .then(function(user){
+            navigate('PList')
+        }).catch(function(error){
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if(errorCode === 'auth/user-not-found') {
+                alert('User not found.');
+            }
+            else if(errorCode === 'auth/wrong-password') {
+                alert('Wrong Password.');
+            }
+            else if(errorCode === 'auth/invalid-email') {
+                alert('Invalid Email.');
+            }
+            else{
+                alert(errorMessage);
+            }
+            console.log(error);
+        });
+        
+    }
+
+
 	render() {
 		const {navigate} = this.props.navigation;
 		return (
@@ -31,6 +72,8 @@ export default class NutritionistSignIn extends Component<{}> {
                         autoCapitalize = "none"
                         autoCorrect = {false}
                         style={styles.input}
+                        onChangeText={(text) => this.setState({email: text})}
+                        value={this.state.email}
                     />
                     <TextInput
                         placeholder={"Enter Password"}
@@ -38,11 +81,13 @@ export default class NutritionistSignIn extends Component<{}> {
                         secureTextEntry
                         style={styles.input}
                         ref={(input) => this.passwordInput = input}
+                        onChangeText={(text) => this.setState({password: text})}
+                        value={this.state.password}
                     />
 
                     <SeafoamButton
                         title="LOGIN"
-                        onPress={() => navigate('PList')}
+                        onPress = {() => this._signIn()}
                     />
 
                 </View>
--- screens/PatientSignIn.js
+++ screens/PatientSignIn.js
@@ -16,7 +16,47 @@ export default class PatientSignIn extends Component<{}> {
 
 	static navigationOptions = {title: 'Please input your login credentials',};
 
-	render() {
+    constructor(props){
+        super(props);
+        this.state =  {email: '', password: ''};
+    }
+
+    _signIn() {
+        var email = this.state.email;
+        var password = this.state.password;
+        const {navigate} = this.props.navigation;
+        if (email.length < 4) {
+            alert('Please enter a valid email address.');
+        }
+        if (password.length < 4) {
+            alert('Please enter a valid password');
+        }
+        //Log in user if correct credentials are entered
+        firebaseApp.auth().signInWithEmailAndPassword(email, password).then(function(user) {
+            navigate('PHome')
+        }).catch(function (error) {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            if (errorCode === 'auth/user-not-found') {
+                alert('User not found.');
+            }
+            else if (errorCode === 'auth/wrong-password') {
+                alert('Wrong Password.');
+            }
+            else if (errorCode === 'auth/invalid-email') {
+                alert('Invalid Email.');
+            }
+            else {
+                alert(errorMessage);
+            }
+            console.log(error);
+        });
+        //after sign in, navigate to patient home screen
+    }
+
+
+
+    render() {
         const {navigate} = this.props.navigation;
         return (
             <View style={styles.container}>
@@ -32,6 +72,8 @@ export default class PatientSignIn extends Component<{}> {
                         autoCapitalize = "none"
                         autoCorrect = {false}
                         style={styles.input}
+                        onChangeText={(text) => this.setState({email: text})}
+                        value={this.state.email}
                     />
                     <TextInput
                         placeholder={"Enter Password"}
@@ -39,11 +81,13 @@ export default class PatientSignIn extends Component<{}> {
                         secureTextEntry
                         style={styles.input}
                         ref={(input) => this.passwordInput = input}
+                        onChangeText={(text) => this.setState({password: text})}
+                        value={this.state.password}
                     />
 
                     <SeafoamButton
                         title="LOGIN"
-			onPress={() => navigate('PHome')}
+                        onPress = {() => this._signIn()}
                     />
 
                 </View>
