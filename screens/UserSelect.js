6b3bfde6cd94be59a52dc9bb86328bebf3d39188
Conner Roberts <31377770+connerlr@users.noreply.github.com>
2017-12-05 17:38:47 -0500

    formatting

--- screens/UserSelect.js
+++ screens/UserSelect.js
@@ -18,41 +18,46 @@ export default class UserSelect extends Component<{}> {
 		const {navigate} = this.props.navigation;
 		return (
         	<View style={styles.container}>
-       			<SeafoamButton 
-				title="Sign into a patient account" 
-				onPress={() => navigate('PSign')}
-				/>
-				<SeafoamButton 
-				title="Sign into a nutritionist account" 
-				onPress={() => navigate('NSign')}
-				/>
-				<SeafoamButton 
-				title="Create new user" 
-				onPress={() => navigate('NewUser')} 
-				/>
-      		</View> 	
+                <Text style={styles.input}>
+                    AahaRX{"\n"}For{"\n"}Diabetes{"\n"}Management  
+                </Text>
+                    <SeafoamButton
+                        style={styles.button}
+                        title="Sign into a patient account"
+                        onPress={() => navigate('PSign')}
+                    />
+                    <Text style={{marginBottom: 30}}>
+                    </Text>
+                    <SeafoamButton
+                        title="Sign into a nutritionist account"
+                        onPress={() => navigate('NSign')}
+                    />
+                    <Text style={{marginBottom: 30}}>
+                    </Text>
+                    <SeafoamButton
+                        title="Create new user"
+                        onPress={() => navigate('NewUser')}
+                    />
+      		</View>
     	);
   	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-	height:80,
-    //alignItems: 'center',
-    backgroundColor: '#F7F1D2',
-      paddingHorizontal: 20,
-      padding: 20
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: '#F7F1D2',
+},
+    input:{
+        fontSize: 40,
+        fontWeight: 'bold',
+        marginBottom: 30,
+        textAlign: 'center',
+        alignSelf: 'stretch',
+        color: "#000000",
+    },
 });
+
