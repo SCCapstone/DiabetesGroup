5f7b3f5a78aa5cda1cd6ae3ef6395472c2d7d88d
Brandon Lood <31374750+bslood@users.noreply.github.com>
2017-12-07 19:23:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import PatientList from './screens/PatientList';
+import { AppRegistry } from 'react-native';
+import { Text } from 'react-native';
+import React from 'react';
+import App from './App';
+import PatientList from './screens/PatientList';
 import UserSelect from './screens/UserSelect';
 import PatientSignIn from './screens/PatientSignIn';
 import NutritionistSignIn from './screens/NutritionistSignIn';
@@ -9,9 +14,11 @@ import patientHome from './screens/patientHome';
 import glucoseInput from './screens/glucoseInput';
 import patientMedication from './screens/patientMedication';
 import patientDiet from './screens/patientDiet';
+import SignOut from './screens/SignOut';
 
 
 import {StackNavigator,} from 'react-navigation';
+import {DrawerNavigator,} from 'react-navigation';
 import NewPatientInfo from "./screens/NewPatientInfo";
 
 //This is going to be the apps main navigator
@@ -20,11 +27,26 @@ const AppNavigator = StackNavigator({
         PSign: {screen: PatientSignIn},
         NSign: {screen: NutritionistSignIn},
         NewUser: {screen: createNewUser},
-        PList: {screen: PatientList},
-        PHome: {screen: patientHome},
+        PList: {
+            screen: PatientList,
+            navigationOptions: ({navigation}) => ({
+                headerStyle: {backgroundColor: '#FF6127'},
+                title: 'Nutritionist Home Screen',
+                headerLeft: <Text onPress={() => navigation.navigate('DrawerOpen')}>Menu</Text>
+            })
+        },
+        PHome: {
+            screen: patientHome,
+            navigationOptions: ({navigation}) => ({
+                headerStyle: {backgroundColor: '#FF6127'},
+                title: 'Home Screen',
+                headerLeft: <Text onPress={() => navigation.navigate('DrawerOpen')}>Menu</Text>
+            })
+        },
         GInput: {screen: glucoseInput},
         PMed: {screen: patientMedication},
         PDiet: {screen: patientDiet},
+        Sign: {screen: SignOut},
     },
     {
 
@@ -33,4 +55,25 @@ const AppNavigator = StackNavigator({
     }
 );
 
-AppRegistry.registerComponent('DiabetesGroup', () => AppNavigator);
+const DrawerStack = DrawerNavigator({
+        GInput: {screen: glucoseInput},
+        PMed: {screen: patientMedication},
+        Plist: {screen: PatientList},
+        Sign: {screen: SignOut},
+        Stack: {
+            screen: AppNavigator,
+            navigationOptions: ({navigation}) => ({
+                drawerLockMode: 'locked-closed',
+                drawerLabel: () => null
+            })
+        },
+    },
+    {
+        drawerPosition: 'left',
+        initialRouteName: 'Stack'
+
+    }
+);
+
+
+AppRegistry.registerComponent('DiabetesGroup', () => DrawerStack);
