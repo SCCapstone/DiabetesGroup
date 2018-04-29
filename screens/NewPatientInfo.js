79c97b771621755a381b240587f681369de0e725
IanD96 <idhistory@zoho.com>
2018-04-29 14:26:40 -0400

    Updated new patient info screen to be consisten with the new settings screen

--- screens/NewPatientInfo.js
+++ screens/NewPatientInfo.js
@@ -2,16 +2,15 @@
 import React, { Component } from 'react';
 const SeafoamButton = require('../components/SeafoamButton');
 import firebaseApp from './FireBaseApp';
+import {WheelPicker} from 'react-native-wheel-picker-android';
+import  Modal from 'react-native-modal';
 import {
-    Platform,
     StyleSheet,
     Text,
     TextInput,
     View,
-    Button,
-    Picker,
-    AppRegistry,
     TouchableOpacity,
+    ScrollView,
 } from 'react-native';
 
 export default class NewPatientInfo extends Component<{}> {
@@ -29,9 +28,8 @@ export default class NewPatientInfo extends Component<{}> {
         this.state = {Age: '', Sex: '', Weight: '', Height: '', DType: ''};
     }
 
+    //function for submitting changed info and updating firebase
     _submitInfo() {
-        const {navigate} = this.props.navigation;
-
         var Age = this.state.Age;
         var Sex = this.state.Sex;
         var Weight = this.state.Weight;
@@ -40,101 +38,194 @@ export default class NewPatientInfo extends Component<{}> {
 
         var user = firebaseApp.auth().currentUser;
         var database = firebaseApp.database();
-        if( Age == '' ||   Weight == '' ){
-            alert('Please enter a value for all fields');
-        }
-        else {
 
-            firebaseApp.database().ref('Patients/' + user.uid + '/Pinfo/').set({
-                Age: Age,
-                Sex: Sex,
-                Weight: Weight,
-                Height: Height,
-                DType: DType,
-            });
-
-            navigate('PHome')
-        }
+        firebaseApp.database().ref('Patients/' + user.uid + '/Pinfo/').set({
+            Age: Age,
+            Sex: Sex,
+            Weight: Weight,
+            Height: Height,
+            DType: DType,
+        });
+        alert('Settings updated.');
+        const {navigate} = this.props.navigation;
+        navigate('PHome')
     }
 
+    //These are functions for rendering the button to close the modals
+    renderButton = (text, onPress) => (
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.button}>
+                <Text style={{color: '#000000'}}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+
+    renderModalButton = (text, onPress) => (
+        <TouchableOpacity onPress={onPress}>
+            <View style={styles.modalButton}>
+                <Text style={{color: '#000000'}}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+
+    //this function checks the text input for letters which aren't valid
+    checkNumberInput(text, type) {
+        var newText = '';
+        var numbers = '0123456789';
+        if(text.length < 1){
+            if(type === 'age') {
+                this.setState({Age: ''});
+            }else{
+                this.setState({Weight: ''});
+            }
+        }
+        for (var i=0; i < text.length; i++) {
+            if(numbers.indexOf(text[i]) > -1 ) {
+                newText = newText + text[i];
+            }else {
+                alert('Please only enter in numbers')
+            }
+            if(type === 'age'){
+                this.setState({Age: newText });
+            }else{
+                this.setState({Weight: newText });
+            }
+        }
+    }
     render() {
+        const GenderData = ['Male', 'Female', 'N/A'];
+        const HeightData = [];
+        for(var i = 4; i<7; i++) {
+            for(var j = 0; j<12; j++){
+                HeightData.push(i + "'" + j + "''");
+            }
+        }
+        const TypeData = ['Type 1 Diabetes', 'Type 2 diabetes', 'Prediabetes', 'Gestational Diabetes'];
+        let tempGender = this.state.Sex;
+        let tempHeight = this.state.Height;
+        let tempDType = this.state.DType;
         return (
-
-            <View style={styles.container}>
-                <View style={styles.stretched}>
-
-                    <Text style={styles.title}>
-                        Enter your information below:
+            <ScrollView style={{backgroundColor: '#FFFCF6'}}>
+                <View style={styles.container}>
+                    <Text style={[styles.title, {marginBottom: 30, marginTop: 40}]}>
+                        Enter Your Personal Information:
                     </Text>
 
-                    <Text>Age:</Text>
-                    <TextInput style={styles.input} placeholder="Age"
-                               underlineColorAndroid ={'transparent'}
-                               placeholderTextColor= "#CFCFCF"
-                               onChangeText={(text) => this.setState({Age: text})}
-                               value={this.state.Age}
-                    />
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: 30}}>
+                        <Text style={{marginRight: 52}}>Age:</Text>
+                        <TextInput style={styles.input} defaultValue= {this.state.Age}
+                                   underlineColorAndroid ={'transparent'}
+                                   keyboardType = 'numeric'
+                                   placeholderTextColor= "#CFCFCF"
+                                   onChangeText={(text) => this.checkNumberInput(text,'age')}
+                                   maxLength={3}
+                                   value={this.state.Age}
+                        />
 
-                    <Text>Gender:</Text>
-                    <Picker
-                        selectedValue = {this.state.Sex}
-                        style={{marginBottom: 15}}
-                        onValueChange={(itemValue) => this.setState({Sex: itemValue})}>
-                        <Picker.Item label="Male" value="Male"/>
-                        <Picker.Item label="Female" value="Female" />
-                    </Picker>
+                        <Text>Gender:</Text>
+                        {this.renderButton(this.state.Sex, () => this.setState({visibleModal: 'gender'}))}
+                        <Modal
+                            isVisible={this.state.visibleModal === 'gender'}
+                            animationIn="slideInLeft"
+                            animationOut="slideOutRight"
+                            onBackdropPress={() => this.setState({visibleModal: null})}
+                        >
+                            <View style={styles.modalContent}>
+                                <WheelPicker
+                                    onItemSelected={(event) => tempGender = event.data}
+                                    selectedItemPosition={GenderData.indexOf(this.state.Sex)}
+                                    isCurved
+                                    isCurtain
+                                    renderIndicator
+                                    indicatorColor='#112471'
+                                    curtainColor='#112471BF'
+                                    selectedItemTextColor='#000000'
+                                    data={GenderData}
+                                    style={{width: 300, height: 300}}
+                                />
+                                {this.renderModalButton('Confirm', () => this.setState({Sex: tempGender,visibleModal: null}))}
+                            </View>
+                        </Modal>
+                    </View>
 
-                    <Text>Weight:</Text>
-                    <TextInput style={styles.input} placeholder = "Weight"
-                               underlineColorAndroid ={'transparent'}
-                               placeholderTextColor= "#CFCFCF"
-                               onChangeText={(text) => this.setState({Weight: text})}
-                               value={this.state.Weight}
-                    />
-                    <Text>Height:</Text>
-                    <Picker
-                        selectedValue = {this.state.Height}
-                        style={{marginBottom: 15}}
-                        onValueChange={(itemValue) => this.setState({Height: itemValue})}>
-                        <Picker.Item label="5'0''" value="5'0''"/>
-                        <Picker.Item label="5'1''" value="5'1''"/>
-                        <Picker.Item label="5'2''" value="5'2''"/>
-                        <Picker.Item label="5'3''" value="5'3''"/>
-                        <Picker.Item label="5'4''" value="5'4''"/>
-                        <Picker.Item label="5'5''" value="5'5''"/>
-                        <Picker.Item label="5'6''" value="5'6''"/>
-                        <Picker.Item label="5'7''" value="5'7''"/>
-                        <Picker.Item label="5'8''" value="5'8''"/>
-                        <Picker.Item label="5'9''" value="5'9''"/>
-                        <Picker.Item label="5'10''" value="5'10''"/>
-                        <Picker.Item label="5'11''" value="5'11''"/>
-                        <Picker.Item label="6'0''" value="6'0''"/>
-                        <Picker.Item label="6'1''" value="6'1''"/>
-                        <Picker.Item label="6'2''" value="6'2''"/>
-                        <Picker.Item label="6'3''" value="6'3''"/>
-                        <Picker.Item label="6'4''" value="6'4''"/>
-                        <Picker.Item label="6'5''" value="6'5''"/>
-                        <Picker.Item label="6'6''" value="6'6''"/>
-                        <Picker.Item label="6'7''" value="6'7''"/>
-                    </Picker>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', marginBottom: 30}}>
+                        <Text>Weight (lbs):</Text>
+                        <TextInput style={styles.input} defaultValue= {this.state.Weight}
+                                   underlineColorAndroid ={'transparent'}
+                                   keyboardType = 'numeric'
+                                   placeholderTextColor= "#CFCFCF"
+                                   onChangeText={(text) => this.checkNumberInput(text,'weight')}
+                                   maxLength={3}
+                                   value={this.state.Weight}
+                        />
 
-                    <Text>Diabetes Type:</Text>
-                    <Picker
-                        selectedValue = {this.state.DType}
-                        style={{marginBottom: 15}}
-                        onValueChange={(itemValue) => this.setState({DType: itemValue})}>
-                        <Picker.Item label="Type 1 Diabetes" value="Type 1 Diabetes"/>
-                        <Picker.Item label="Type 2 Diabetes" value="Type 2 Diabetes" />
-                        <Picker.Item label="Prediabetes" value="Prediabetes" />
-                        <Picker.Item label="Gestational Diabetes" value="Gestational Diabetes" />
-                    </Picker>
+                        <Text>Height:</Text>
+                        {this.renderButton(this.state.Height, () => this.setState({visibleModal: 'height'}))}
+                        <Modal
+                            isVisible={this.state.visibleModal === 'height'}
+                            animationIn="slideInLeft"
+                            animationOut="slideOutRight"
+                            onBackdropPress={() => this.setState({visibleModal: null})}
+                        >
+                            <View style={styles.modalContent}>
+                                <WheelPicker
+                                    onItemSelected={(event) => tempHeight = event.data}
+                                    selectedItemPosition={HeightData.indexOf(this.state.Height)}
+                                    isCurved
+                                    isCurtain
+                                    renderIndicator
+                                    indicatorColor='#112471'
+                                    curtainColor='#112471BF'
+                                    selectedItemTextColor='#000000'
+                                    data={HeightData}
+                                    style={{width: 300, height: 300}}
+                                />
+                                {this.renderModalButton('Confirm', () => this.setState({Height: tempHeight, visibleModal: null}))}
+                            </View>
+                        </Modal>
+                    </View>
+
+                    <View style={{alignItems: 'center'}}>
+                        <View style={{flexDirection: 'row', JustifyContent: 'center', marginBottom: 30, textAlign: 'center'}}>
+                            <Text style={{marginRight: 10}}>Diabetes Type:</Text>
+                            <TouchableOpacity onPress={() => this.setState({visibleModal: 'DType'})}>
+                                <View style={styles.DTbutton}>
+                                    <Text style={{color: '#000000'}}>{this.state.DType}</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <Modal
+                                isVisible={this.state.visibleModal === 'DType'}
+                                animationIn="slideInLeft"
+                                animationOut="slideOutRight"
+                                onBackdropPress={() => this.setState({visibleModal: null})}
+                            >
+                                <View style={styles.modalContent}>
+                                    <WheelPicker
+                                        onItemSelected={(event) => tempDType = event.data}
+                                        selectedItemPosition={TypeData.indexOf(this.state.DType)}
+                                        isCurved
+                                        isCurtain
+                                        renderIndicator
+                                        indicatorColor='#112471'
+                                        curtainColor='#112471BF'
+                                        selectedItemTextColor='#000000'
+                                        data={TypeData}
+                                        style={{width: 300, height: 300}}
+                                    />
+                                    {this.renderModalButton('Confirm', () => this.setState({DType: tempDType, visibleModal: null}))}
+                                </View>
+                            </Modal>
+                        </View>
+                    </View>
 
                     <SeafoamButton
-                        title="Submit"
+                        title="Submit Info"
                         onPress = {() => this._submitInfo()}
                     />
+
                 </View>
-            </View>
+
+            </ScrollView>
         );
     }
 }
@@ -142,9 +233,8 @@ export default class NewPatientInfo extends Component<{}> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        paddingLeft: 55,
-        paddingRight: 55,
+        paddingLeft: 25,
+        paddingRight: 25,
         backgroundColor: '#fffcf6',
     },
     submitbutton:{
@@ -155,20 +245,60 @@ const styles = StyleSheet.create({
     submittext:{
         fontWeight: 'bold',
     },
-    stretched: {
-        alignSelf: 'stretch',
-    },
     title: {
         fontSize: 20,
+        fontWeight: 'bold',
         textAlign: 'center',
         color: "#000000",
     },
     input:{
-        marginBottom: 10,
-        alignSelf: 'stretch',
         backgroundColor: '#ffffff',
         borderColor: "#000000",
         borderWidth: 1,
+        height: 45,
+        width: 80,
+        textAlign: 'center'
     },
-
+    passInput:{
+        width: 200,
+        backgroundColor: '#ffffff',
+        borderColor: "#000000",
+        borderWidth: 1,
+        padding: 12,
+    },
+    button: {
+        backgroundColor: "#ffffff",
+        justifyContent: "center",
+        height: 45,
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "#000000",
+        width: 80,
+    },
+    modalButton: {
+        backgroundColor: "#059c29",
+        padding: 12,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 4,
+        borderColor: "rgba(0, 0, 0, 0.1)",
+        marginBottom: 15,
+    },
+    DTbutton: {
+        backgroundColor: "#ffffff",
+        padding: 12,
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "#000000",
+        marginBottom: 15,
+        minWidth: 100,
+    },
+    modalContent: {
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 4,
+        borderColor: "rgba(0, 0, 0, 0.1)"
+    },
 });
